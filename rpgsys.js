d0afa48f56046cefec5261219c253bffbc157034
jordanforbes <Jordforb@gmail.com>
2017-08-17 12:28:26 -0400

    created constructor

--- rpgsys.js
+++ rpgsys.js
@@ -19,37 +19,64 @@ function measureClockSystem(){
 
     setClock(beat)
     console.log(beat)
-    fireBall(beat)
-    thunder(beat)
+    move(thunderMag)
+    move(fireMag)
+    move(iceMag)
   })
 }
 
 //attacks
-function fireBall(beat){
-  if(beat==2){
-    console.log("fireball " + beat)
-  }
+
+function attack(name, startInterval, currentInterval, damage){ //attack constructor
+  this.name= name;
+  this.startInterval= startInterval;
+  this.currentInterval = currentInterval;
+  this.damage = damage;
 }
 
-interval = 5
+var iceMag = new attack("Ice", 7, 7, 7)
+var thunderMag = new attack("Thunder", 5,5,5)
+var fireMag = new attack("FireBall",3,3,3)
 
-function thunder(beat){
+function move(attack){ //use move
 
-  function check(){
-
-    if(beat==interval){
-      console.log("thunder " + beat)
-
-      interval= interval+5
-      if(interval>8){
-         interval= interval - 8
-      }
-      console.log("new interval "+interval)
+  if(beat == attack.currentInterval){
+    console.log(attack.name + " " + beat)
+    attack.currentInterval = attack.currentInterval + attack.startInterval
+    if(attack.currentInterval>8){
+      attack.currentInterval = attack.currentInterval -8
     }
-
+    console.log(attack.name+ " is now "+ attack.currentInterval)
   }
-  check()
+
 }
+
+
+// function fireBall(beat){
+//   if(beat==2){
+//     console.log("fireball " + beat)
+//   }
+// }
+//
+// interval = 5
+//
+// function thunder(beat){
+//
+//   function check(){
+//
+//     if(beat==interval){
+//       console.log("thunder " + beat)
+//
+//       interval= interval+5
+//       if(interval>8){
+//          interval= interval - 8
+//       }
+//       console.log("new interval "+interval)
+//     }
+//
+//   }
+//   check()
+// }
 
 
 
